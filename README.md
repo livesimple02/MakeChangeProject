3cd6e6a4266e789b058d1322fb0fe8a294ba5fec
livesimple02 <shlaupan@yahoo.com>
2021-05-14 18:31:27 -0600

    Final Submit for MakeChange Project. Fully Tested

--- README.md
+++ README.md
@@ -2,15 +2,39 @@
 
 ### Overview
 
-This program simulates a cash register and provides change to customers in the
-appropriate currency.
+Given a price and dollar amount paid, this program will return to the user
+the change required using the least amount of dollar bills and coins.
 
 ### Technologies Used
 
 -Java
 -Conditionals
 -Looping
+-Switch
+-Type Casting
+-Method Calling with returns
+-Float Formatting
 
 ### How to Run
 
+The program will prompt for a dollar amount to be paid followed by the dollar
+amount tendered. Values entered here must be positive numbers with up to two
+decimal places.
+
+The program will return the change that would need to be provided using the
+least number of bills and coins.
+
+Following a transaction, the program will prompt the user for whether they would
+like to perform another transaction.
+Acceptable user responses here include:
+  'Y' or 'y' to perform another transaction.
+  'N' or 'n' to exit the program.
+  Any other entry will notify the user and wait for another entry.
+
 ### Lessons Learned
+
+When fixing errors to account for all possible outcomes, if the program seems
+like it is getting too complicated for a simple task, it probably is. Step away
+and think of other methods to come to a solution.
+
+Refractor! Refractor! Refractor!
--- src/com/skilldistillery/MakeChange.java
+++ src/com/skilldistillery/MakeChange.java
@@ -1,5 +1,169 @@
 package com.skilldistillery;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
-}
+	static Scanner input = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		// Initialize variables for main
+		float price = 0;
+		float paid = 0;
+		int changeNeeded = 0;
+		boolean keepGoing = true;
+		
+		// Do-while loop allows for multiple transactions
+		do {
+			String finalResultStr = "";
+			
+			// Get values from user
+			System.out.print("Enter the total cost: ");
+			price = input.nextFloat();
+			System.out.print("Enter the amount paid: ");
+			paid = input.nextFloat();
+			
+			// Convert from float to integer. + 0.001 allows for inaccuracies in float variable to avoid truncating a penny
+			changeNeeded = (int)(((paid - price)+.001)*100);
+			
+			System.out.printf("\nAmount Due: $%.2f | Amount Tendered: $%.2f\n\n    Change Due\n------------------\n" , price, paid);
+			
+			// Check to see if any change is needed
+			if (changeNeeded < 0) {
+				System.out.println("Not enough money tendered! Please try again.\n");
+			}
+			
+			else if (changeNeeded == 0) {
+				System.out.println("$0 - Thank you for paying the exact amount!\n");
+			}
+			
+			// If change needed, build final output string by checking each denomination and reducing change needed as applicable
+			else {
+				finalResultStr += getChange(changeNeeded, 2000);
+				changeNeeded = changeNeeded % 2000;
+				
+				finalResultStr += getChange(changeNeeded, 1000);
+				changeNeeded = changeNeeded % 1000;
+				
+				finalResultStr += getChange(changeNeeded, 500);
+				changeNeeded = changeNeeded % 500;
+				
+				finalResultStr += getChange(changeNeeded, 100);
+				changeNeeded = changeNeeded % 100;
+				
+				finalResultStr += getChange(changeNeeded, 25);
+				changeNeeded = changeNeeded % 25;
+				
+				finalResultStr += getChange(changeNeeded, 10);
+				changeNeeded = changeNeeded % 10;
+				
+				finalResultStr += getChange(changeNeeded, 5);
+				changeNeeded = changeNeeded % 5;
+				
+				finalResultStr += getChange(changeNeeded, 1);
+				
+				System.out.println(finalResultStr + "------------------");
+			}
+			
+			// Check to see if user wants to perform additional transactions
+			keepGoing = anotherTransact();
+			
+		} while (keepGoing);
+		
+		input.close();
+		
+	} // End of Main Method
+
+	
+	
+	
+	// Method to determine number of bills or coins needed by denomination value passed from main.
+	// First IF statement is specific to pennies to make sure stray pennies are not forgotten about due to inaccuracies associated with float or double
+	public static String getChange(int changeNeeded, int denomination) {
+		
+		int billCoinQty = changeNeeded / denomination;
+		
+		if (billCoinQty == 0) {
+			return "";
+		}
+			
+		else {
+			return (getDenominationDesc(denomination) + billCoinQty + "  |\n");
+		}
+						
+	} // End of getChange method
+	
+	
+	
+	// Method to return description based on denomination
+	public static String getDenominationDesc(int denomination) {
+		
+		String denominationDesc = "";
+		
+		switch (denomination) {
+		case 2000:
+			denominationDesc = "| $20 bills : ";
+			break;
+		case 1000:
+			denominationDesc = "| $10 bills : ";
+			break;
+		case 500:
+			denominationDesc = "|  $5 bills : ";
+			break;
+		case 100:
+			denominationDesc = "|  $1 bills : ";
+			break;
+		case 25:
+			denominationDesc = "|  Quarters : ";
+			break;
+		case 10:
+			denominationDesc = "|     Dimes : ";
+			break;
+		case 5:
+			denominationDesc = "|   Nickels : ";
+			break;
+		case 1:
+			denominationDesc = "|   Pennies : ";
+		}
+		return denominationDesc;
+		
+	}	// End of getDenominationDesc method
+	
+	
+	
+	// Method to check if user wants to perform another transaction
+	public static boolean anotherTransact() {
+		
+		boolean waitForResponse = true;
+		boolean keepGoing = true;
+		
+		System.out.print("Would you like to perform another transaction? Press 'Y' or 'N': ");
+		
+		while (waitForResponse) {
+			
+			String userResponse = input.next();
+			
+			if (userResponse.equalsIgnoreCase("N")) {
+				System.out.println("\nGoodbye");
+				waitForResponse = false;
+				keepGoing = false;
+			}
+			
+			else if (userResponse.equalsIgnoreCase("Y")) {
+				System.out.println();
+				waitForResponse = false;
+				keepGoing = true;
+			}
+			
+			else {
+				System.out.print("\nCommand not recognized, please enter 'Y' or 'N': ");
+			}
+			
+		}
+		return keepGoing;
+		
+	} // End of anotherTransact method
+	
+	
+} // End of Class
